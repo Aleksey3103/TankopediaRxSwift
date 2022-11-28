05949a34f2105d7ddadcd6de07c48622f3ceb429
Aleksey <alekse310399@gmail.com>
2022-11-28 18:00:02 +0200

    small fix

--- NetworkTanks/RequestManager.swift
+++ NetworkTanks/RequestManager.swift
@@ -31,38 +31,37 @@ class RequestManager {
             } catch {
                 print("CATCH ERROR \(error.localizedDescription)")
                 tanks.onError(error)
-//                tanks.onError(error.localizedDescription as! Error)
-            }
-            
+                //                tanks.onError(error.localizedDescription as! Error)
+            } 
         }
         task.resume()
-//        let session = URLSession.shared
-//        let urlOptional = URL(string:"https://api.worldoftanks.ru/wot/encyclopedia/vehicles/?application_id=8597d726286d88724d624cd07e8aed4f&limit=10&page_no=1")
+        //        let session = URLSession.shared
+        //        let urlOptional = URL(string:"https://api.worldoftanks.ru/wot/encyclopedia/vehicles/?application_id=8597d726286d88724d624cd07e8aed4f&limit=10&page_no=1")
         
-//        if let url = urlOptional {
-//            let task = session.dataTask(with: url) { [self] (data, responce, error) in
-//                if let error = error {
-//                    print("DataTask error\(String(describing: error.localizedDescription))")
-//                    tankDataError?(error.localizedDescription)
-//                    return
-//                }
-//                do {
-//                    if let data = data {
-//                        self.tankListData = try JSONDecoder().decode(ModelTanks.self, from: data)
-//                        print("Tank DATA \(tankListData)")
-//                        tankListDataSuccsesClosure?(tankListData)
-//                    }
-//                } catch {
-//                    print("CATCH ERROR \(error.localizedDescription)")
-//                    tankDataError?(error.localizedDescription)
-//                }
-//                DispatchQueue.main.async { [weak self] in
-//                    if let tankListData = self?.tankListData {
-//                        self?.tankListDataSuccsesClosure?(tankListData)
-//                    }
-//                }
-//            }
-//            task.resume()
-//        }
+        //        if let url = urlOptional {
+        //            let task = session.dataTask(with: url) { [self] (data, responce, error) in
+        //                if let error = error {
+        //                    print("DataTask error\(String(describing: error.localizedDescription))")
+        //                    tankDataError?(error.localizedDescription)
+        //                    return
+        //                }
+        //                do {
+        //                    if let data = data {
+        //                        self.tankListData = try JSONDecoder().decode(ModelTanks.self, from: data)
+        //                        print("Tank DATA \(tankListData)")
+        //                        tankListDataSuccsesClosure?(tankListData)
+        //                    }
+        //                } catch {
+        //                    print("CATCH ERROR \(error.localizedDescription)")
+        //                    tankDataError?(error.localizedDescription)
+        //                }
+        //                DispatchQueue.main.async { [weak self] in
+        //                    if let tankListData = self?.tankListData {
+        //                        self?.tankListDataSuccsesClosure?(tankListData)
+        //                    }
+        //                }
+        //            }
+        //            task.resume()
+        //        }
     }
 }
